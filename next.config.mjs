8f2dc4be5113903a0d9be4dc981c310ebd39a876
CoG-Analytics <andrewdmerrill@gmail.com>
2026-03-05 21:51:18 -0600

    Ran Build; Attempted Prep for Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: "export",
+  images: {unoptimized: true},
+  basePath: "/collage-generator",
+  assetPrefix: "/collage-generator/"
 };
 
 export default nextConfig;
